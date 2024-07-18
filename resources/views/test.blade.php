8ac520702e941a847e39adc99aa56ceea541051c
Andy Wei <48671893+Andy0909@users.noreply.github.com>
2024-07-18 13:21:06 +0800

    test css load

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <link rel="stylesheet" href="{{ asset('css/test.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/test.css') }}">
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
