c995aeedf026d1fd2ad011f7da057e81c9c69a1c
Andy Wei <48671893+Andy0909@users.noreply.github.com>
2024-07-24 16:38:17 +0800

    update css load

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo asset('css/style.css') ?>">
     <!-- Multi Select-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag/dist/css/multi-select-tag.css?v={{ time() }}">
     <!-- Bootstrap core JS-->
