c43ab1b289754fca1d6706841ad1949c95075231
Andy Wei <48671893+Andy0909@users.noreply.github.com>
2024-08-12 20:22:11 +0800

    調整 css 引入

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -18,52 +18,7 @@
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js?v={{ time() }}"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js?v={{ time() }}"></script>
+    <script href="{{ asset('js/script.js') }}"></script>
     <!-- Core theme JS-->
-    <script>    
-        window.addEventListener('DOMContentLoaded', event => {
-
-            // Navbar shrink function
-            var navbarShrink = function () {
-                const navbarCollapsible = document.body.querySelector('#mainNav');
-                if (!navbarCollapsible) {
-                    return;
-                }
-                if (window.scrollY === 0) {
-                    navbarCollapsible.classList.remove('navbar-shrink')
-                } else {
-                    navbarCollapsible.classList.add('navbar-shrink')
-                }
-
-            };
-
-            // Shrink the navbar 
-            navbarShrink();
-
-            // Shrink the navbar when page is scrolled
-            document.addEventListener('scroll', navbarShrink);
-
-            // Activate Bootstrap scrollspy on the main nav element
-            const mainNav = document.body.querySelector('#mainNav');
-            if (mainNav) {
-                new bootstrap.ScrollSpy(document.body, {
-                    target: '#mainNav',
-                    offset: 72,
-                });
-            };
-
-            // Collapse responsive navbar when toggler is visible
-            const navbarToggler = document.body.querySelector('.navbar-toggler');
-            const responsiveNavItems = [].slice.call(
-                document.querySelectorAll('#navbarResponsive .nav-link')
-            );
-            responsiveNavItems.map(function (responsiveNavItem) {
-                responsiveNavItem.addEventListener('click', () => {
-                    if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                        navbarToggler.click();
-                    }
-                });
-            });
-        });
-    </script>
     <script src="https://cdn.startbootstrap.com/sb-forms-latest.js?v={{ time() }}"></script>
 </head>
