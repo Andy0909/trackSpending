4ae255f4ef0b304354f87de55a1bede7ff4e53e5
Andy Wei <48671893+Andy0909@users.noreply.github.com>
2024-08-22 14:28:30 +0800

    優化分帳邏輯

--- app/Services/CalculateAveragePriceService.php
+++ app/Services/CalculateAveragePriceService.php
@@ -7,7 +7,7 @@ class CalculateAveragePriceService
     /**
      * 計算分攤金額
      * @param array $formattedItems
-     * @return array $result
+     * @return array
      */
     public function calculateAveragePrice(array $formattedItems): array
     {
@@ -19,6 +19,7 @@ class CalculateAveragePriceService
             $shareMembers = $item['shareMember'];
             $averagePrice = round($totalPrice / count($shareMembers), 2); // 每個人分攤的金額
 
+            // 計算成員間的欠款
             foreach ($shareMembers as $member) {
                 if ($member !== $payer) {
                     if (isset($averageResult[$payer][$member])) {
@@ -32,29 +33,67 @@ class CalculateAveragePriceService
             }
         });
 
-        $count = 0;
-        $maxItemKey = '';
+        if (count($averageResult) > 1) {
 
-        foreach ($averageResult as $key => $item) {
-            if (count($item) > $count) {
-                $count = count($item);
-                $maxItemKey = $key;
-            }
+            // 取得最多成員參與的分攤項目的 key
+            $maxItemKey = $this->getMaxItemKey($averageResult);
+
+            // 調整金額 (若有可相互抵消之項目)
+            $offsetAverageResult = $this->offsetAverage($averageResult, $maxItemKey);
+
+            return $offsetAverageResult;
         }
 
-        foreach ($averageResult as $key => $item) {
+        return $averageResult;
+    }
 
-            foreach ($item as $itemKey => $value) {
+    /**
+     * 取得最多成員參與的分攤項目的 key
+     * @param array $averageResult
+     * @return string
+     */
+    private function getMaxItemKey(array $averageResult): string
+    {
+        $maxItemKey = collect($averageResult)
+            ->keys()
+            ->reduce(function ($carry, $key) use ($averageResult) {
+                return count($averageResult[$key]) > (isset($averageResult[$carry]) ? count($averageResult[$carry]) : 0)
+                    ? $key : $carry;
+            }, '');
 
-                if ($key !== $maxItemKey) {
+        return $maxItemKey;
+    }
+
+    /**
+     * 調整金額 (若有可相互抵消之項目)
+     * @param array $averageResult
+     * @param string $maxItemKey
+     * @return array
+     */
+    private function offsetAverage(array $averageResult, string $maxItemKey): array
+    {
+        collect($averageResult)->each(function ($item, $key) use (&$averageResult, $maxItemKey) {
+
+            if ($key !== $maxItemKey) {
+
+                collect($item)->each(function ($value, $itemKey) use (&$averageResult, $key, $maxItemKey) {
+
                     if (isset($averageResult[$maxItemKey][$key]) && isset($averageResult[$maxItemKey][$itemKey])) {
                         $averageResult[$maxItemKey][$key] -= $value;
                         $averageResult[$maxItemKey][$itemKey] += $value;
                         unset($averageResult[$key][$itemKey]);
+        
+                        if ($averageResult[$maxItemKey][$key] == 0) {
+                            unset($averageResult[$maxItemKey][$key]);
+                        }
+        
+                        if ($averageResult[$maxItemKey][$itemKey] == 0) {
+                            unset($averageResult[$maxItemKey][$itemKey]);
+                        }
                     }
-                }
+                });
             }
-        }
+        });
 
         return $averageResult;
     }
